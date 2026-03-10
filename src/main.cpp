1cc40fbc576244c0c0fc308649fc580f84f185ce
Laurent-Mercier <laurent.mercier@umontreal.ca>
2026-03-10 11:43:20 -0400

    feat: initial parsing of arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <stdexcept>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
 int main(int argc, char* argv[]) {
-    std::cout << "nmap++ v0.1.0" << std::endl;
+    if (argc != 3) {
+        std::cout << "Usage: ./nmappp <ip> <port>" << std::endl;
+        return 1;
+    }
+
+    std::string ip   = argv[1];
+    try {
+        int port = std::stoi(argv[2]);
+    } catch (std::invalid_argument e) {
+        std::cout << "Invalid port" << e.what() << std::endl;
+    }
+    
+
+    std::cout << "Scanning " << ip << " on port " << port << std::endl;
     return 0;
 }
